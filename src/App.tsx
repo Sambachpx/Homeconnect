1161623fb44deefec09ade4454302146bed1312a
Sambachpx <113524274+Sambachpx@users.noreply.github.com>
2023-05-16 16:10:02 +0200

    supp node module

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,29 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import './App.css';
 
 function Header() {
   return (
     <div className="flex flex-row">
-    <img src="../src/images/logoHomeConnect.png" className="w-20 h-30" alt="logo HomeConnect" />
+      <img src="../src/images/logoHomeConnect.png" className="w-20 h-30" alt="logo HomeConnect" />
     </div>
   );
 }
 
 function Navigation() {
-  const handleClick = (event: React.MouseEvent<HTMLAnchorElement, MouseEvent>): void => {
-    event.preventDefault();
+  const history = useHistory();
+
+  const handleNavigation = (path: string): void => {
+    history.push(path);
   };
+
   return (
     <div className="flex flex-row">
-      <a className="mt-2 px-2 py-2 text-grey font-bold rounded-md hover:text-orange-500" href='/App.tsx' onClick={handleClick} role="button"> Accueil</a>
-      <a className="mt-2 px-2 py-2 text-grey font-bold rounded-md hover:text-orange-500" href='/AmpoulesConnectees.tsx' onClick={handleClick} role="button"> Ampoules connectées</a>
-      <a className="mt-2 px-2 py-2 text-grey font-bold rounded-md hover:text-orange-500" href='/prises' onClick={handleClick} role="button"> Prises connectées</a>
-      <a className="mt-2 px-2 py-2 text-grey font-bold rounded-md hover:text-orange-500" href='/serrures' onClick={handleClick} role="button"> Serrures connectées</a>
-      <a className="mt-2 px-2 py-2 text-grey font-bold rounded-md hover:text-orange-500" href='/cameras' onClick={handleClick} role="button"> Caméras connectées</a>
+      <button className="mt-2 px-2 py-2 text-grey font-bold rounded-md hover:text-orange-500" onClick={() => handleNavigation('/')}> Accueil</button>
+      <button className="mt-2 px-2 py-2 text-grey font-bold rounded-md hover:text-orange-500" onClick={() => handleNavigation('/ampoules')}> Ampoules connectées</button>
+      <button className="mt-2 px-2 py-2 text-grey font-bold rounded-md hover:text-orange-500" onClick={() => handleNavigation('/prises')}> Prises connectées</button>
+      <button className="mt-2 px-2 py-2 text-grey font-bold rounded-md hover:text-orange-500" onClick={() => handleNavigation('/serrures')}> Serrures connectées</button>
+      <button className="mt-2 px-2 py-2 text-grey font-bold rounded-md hover:text-orange-500" onClick={() => handleNavigation('/cameras')}> Caméras connectées</button>
     </div>
   );
 }
