0ee5f62248f990feff7ee2e32a6bf6e4f4552db7
Sambachpx <113524274+Sambachpx@users.noreply.github.com>
2023-06-11 01:56:32 +0200

    fix page Ampoules

--- app/Ampoules/page.tsx
+++ app/Ampoules/page.tsx
@@ -1,33 +1,33 @@
 import Image from "next/image";
 export const metadata = {
-  title: "Caméras Connectées",
+  title: "Ampoules Connectées",
 };
 
-export default function Camera() {
+export default function Ampoule() {
   return (
     <>
       <h1 className="text-red-500 text-center text-4xl my-8">
-        Caméras Connectées
+        Ampoules Connectées
       </h1>
       <div className="container mx-auto px-4">
         <p className="text-gray-600 text-center mb-8">
-          Découvrez notre gamme de caméras connectées pour surveiller et
-          sécuriser votre domicile en temps réel.
+          Découvrez notre gamme dampoules connectées pour rendre votre maison
+          intelligente et écoénergétique.
         </p>
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
           <div className="bg-white p-4">
             <Image
-              src={"/camera1.webp"}
+              src={"/ampoule1.jpeg"}
               height={0}
               width={200}
-              alt="image camera1"
+              alt="image ampoule1"
             />
-            <h3 className="text-xl mb-2">Caméra Connectée</h3>
+            <h3 className="text-xl mb-2">Ampoule Connectée</h3>
             <p className="text-gray-500 mb-4">
-              Surveillez votre domicile depuis votre smartphone
+              Ampoule LED intelligente compatible avec Alexa et Google Home
             </p>
             <div className="flex justify-between items-center">
-              <span className="text-lg">99,99 €</span>
+              <span className="text-lg">29,99 €</span>
               <a
                 href="/test/public/ampoule1.jpeg"
                 className="bg-green-500 text-white px-4 py-2"
@@ -39,17 +39,17 @@ export default function Camera() {
 
           <div className="bg-white p-4">
             <Image
-              src={"/camera2.jpeg"}
+              src={"/ampoule2.jpeg"}
               height={0}
               width={200}
-              alt="image camera1"
+              alt="image ampoule2"
             />
-            <h3 className="text-xl mb-2">Caméra Connectée 2</h3>
+            <h3 className="text-xl mb-2">Ampoule Connectée 2</h3>
             <p className="text-gray-500 mb-4">
-              Description de la caméra connectée 2
+              Description de lampoule connectée 2
             </p>
             <div className="flex justify-between items-center">
-              <span className="text-lg">119,99 €</span>
+              <span className="text-lg">39,99 €</span>
               <a
                 href="/test/public/ampoule1.jpeg"
                 className="bg-green-500 text-white px-4 py-2"
@@ -61,17 +61,17 @@ export default function Camera() {
 
           <div className="bg-white p-4">
             <Image
-              src={"/camera3.jpeg"}
+              src={"/ampoule3.webp"}
               height={0}
               width={200}
-              alt="image camera1"
+              alt="image ampoule3"
             />
-            <h3 className="text-xl mb-2">Caméra Connectée 3</h3>
+            <h3 className="text-xl mb-2">Ampoule Connectée 3</h3>
             <p className="text-gray-500 mb-4">
-              Description de la caméra connectée 3
+              Description de lampoule connectée 3
             </p>
             <div className="flex justify-between items-center">
-              <span className="text-lg">139,99 €</span>
+              <span className="text-lg">49,99 €</span>
               <a
                 href="/test/public/ampoule1.jpeg"
                 className="bg-green-500 text-white px-4 py-2"
