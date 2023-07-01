71a32e0cb68e623dc8e06f253fec0f9fac522b8b
Sambachpx <113524274+Sambachpx@users.noreply.github.com>
2023-07-01 16:32:50 +0200

    fix errors remove files

--- app/page.tsx
+++ app/page.tsx
@@ -1,76 +1,90 @@
 import Image from "next/image";
-import Link from "next/link";
 
 export const metadata = {
   title: "Accueil",
 };
-
 export default function Home() {
   return (
-    <div className="bg-black">
-      <main className="flex min-h-screen flex-col items-center justify-between p-24 text-white">
-        <Image
-          src="/geforce.jpg"
-          height={0}
-          width={2000}
-          alt="carte graphique"
-          className="pt-[10px]"
-        />
-
-        <Link href="/commander">
-          <button className="text-black rounded-3xl h-10 w-36 bg-heliotrope-300 border-heliotrope-300 text-s font-bold">
-            Commander
-          </button>
-        </Link>
-
-        <div className="flex space-x-4 m-auto text-center justify-center">
-          <div className="flex flex-col items-center w-[500px]">
-            <div className="flex justify-center">
-              <div className="bg-cover">
-                <Image
-                  src="/batman.jpg"
-                  width={200}
-                  height={600}
-                  alt="écran"
-                  className="w-full h-auto"
-                />
-              </div>
+    <>
+      <h1 className="text-red-500 text-center text-4xl my-8">
+        Bienvenue sur HomeConnect
+      </h1>
+      <div className="container mx-auto px-4">
+        <p className="text-gray-600 text-center mb-8">
+          Découvrez notre sélection dappareils domotiques pour rendre votre
+          maison intelligente et connectée.
+        </p>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
+          <div className="bg-white p-4">
+            <Image
+              src={"/ampoule1.jpeg"}
+              height={0}
+              width={200}
+              alt="image ampoule1"
+              className="w-full h-auto"
+            />
+            <h3 className="text-xl mb-2">Ampoule Connectée</h3>
+            <p className="text-gray-500 mb-4">
+              Ampoule LED intelligente compatible avec Alexa et Google Home
+            </p>
+            <div className="flex justify-between items-center">
+              <span className="text-lg">29,99 €</span>
+              <a
+                href="/test/public/ampoule1.jpeg"
+                className="bg-green-500 text-white px-4 py-2"
+              >
+                ajouter au panier
+              </a>
             </div>
           </div>
-          <div className="flex flex-col items-center w-[350px]">
-            <div className="flex justify-center">
-              <div className="w-[600px]">
-                <Image
-                  src="/pc.png"
-                  width={200}
-                  height={100}
-                  alt="pc gamers"
-                  className="w-full h-auto"
-                />
-              </div>
+          <div className="bg-white p-4">
+            <Image
+              src={"/prise1.jpeg"}
+              height={0}
+              width={200}
+              alt="image prise1"
+              style={{ width: "auto" }}
+            />
+            <h3 className="text-xl mb-2">Prise Connectée</h3>
+            <p className="text-gray-500 mb-4">
+              controlez vos appareils avec votre smartphone
+            </p>
+            <div className="flex justify-between items-center">
+              <span className="text-lg">19,99 €</span>
+              <a
+                href="/test/public/ampoule1.jpeg"
+                className="bg-green-500 text-white px-4 py-2"
+              >
+                Ajouter au panier
+              </a>
             </div>
           </div>
-          +++++++++
+
+          <div className="bg-white p-4">
+            <Image
+              src={"/camera1.webp"}
+              height={0}
+              width={200}
+              alt="image camera1"
+              className="w-full h-auto"
+              priority={true}
+            />
+            <h3 className="text-xl mb-2">Caméra de Sécurité</h3>
+            <p className="text-gray-500 mb-4">
+              Caméra IP sans fil avec vision nocturne et détection de mouvement
+            </p>
+            <div className="flex justify-between items-center">
+              <span className="text-lg">59,99 €</span>
+              <a
+                href="/product/3"
+                className="bg-green-500 text-white px-4 py-2"
+              >
+                Ajouter au panier
+              </a>
+            </div>
+          </div>
         </div>
-
-        <div className="text-center">
-          <div className="flex justify-center">
-            <div className="w-[800px]">
-              <Image
-                src="/piece.jpg"
-                width={600}
-                height={100}
-                alt="pièce graphique"
-                className="w-full h-auto"
-              />
-
-              <button className="text-black rounded-3xl h-10 w-36 bg-heliotrope-300 border-heliotrope-300 text-s font-bold mt-10">
-                Acheter
-              </button>
-            </div>
-          </div>
-        </div>
-      </main>
-    </div>
+      </div>
+    </>
   );
 }
